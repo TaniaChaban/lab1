0415552fe691686d0267f7bc28d3d64a194b2ce7
TaniaChaban <78304013+TaniaChaban@users.noreply.github.com>
2021-02-05 21:00:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
+#include <math.h>
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
-int main() 
+int main(int argc, char** argv) 
 {
-float a,b,c;
-cin>>a>>b;
-c=a+b;
-cout<<"a+b="<<c;
-return 0;
+	int x1, x2, y1, y2, gip;
+	cout<<"x1;y1"<<endl;
+	cin>>x1>>y1; 	
+	cout<<endl;
+	cout<<"x2;y2"<<endl;
+	cin>>x2>>y2; 
+	cout<<endl;
+	gip=sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+	cout<<"result gip="<<gip;
+	//getch();
+	return 0;
 }
